0b1b850baf147e9e552e37e6c3670ca121790d7f
edmond ukpebor <ukpebor.edmond123@gmail.com>
2023-01-13 12:34:05 -0800

    all task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,9 @@
 #ifndef main_h
 #define main_h
 
-/*
- * File: main.h
- * Description: more malloc
- */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
